f408e1b92224a4b8d517b1ae47889f06494fdefc
rrmartinez87 <rafael.martinez.angel@gmail.com>
2020-06-16 05:36:52 -0500

    change

--- variables.tf
+++ variables.tf
@@ -144,3 +144,123 @@ variable "private_dns_zone_vnet_link" {
     type = string
     default = "private_dns_zone_vnet_link"  
 }
+ 
+// Ip of the Virtual machine
+variable "azurerm_public_ip_name" {
+    description = "The name of the Ip"
+    type = string
+    default = "test-pip"  
+}
+variable "azurerm_public_ip_allocation_method" {
+    description = "ip allocation method"
+    type = string
+    default = "Dynamic"  
+}
+variable "azurerm_public_ip_idle_timeout_in_minutes" {
+    description = "timeout in minutes"
+    type = number
+    default = 30
+}
+variable "azurerm_public_ip_enviroment" {
+    description = "ip enviroment"
+    type = string
+    default = "dev"
+}
+// Network Interface of the Virtual machine
+variable "azurerm_network_interface_name" {
+    description = "network interface name"
+    type = string
+    default = "interface"
+}
+variable "ip_configuration_name" {
+    description = "ip configuration name"
+    type = string
+    default = "testconfiguration1"
+}
+variable "private_ip_address_allocation" {
+    description = "private ip address allocation"
+    type = string
+    default = "static"
+}
+variable "private_ip_address" {
+    description = "private ip address allocation"
+    type = string
+    default = "10.0.1.5"
+} 
+// Virtual machine to test connectiom
+variable "azurerm_virtual_machine_name" {
+    description = "(Required) Specifies the name of the Virtual Machine. Changing this forces a new resource to be created."
+    type = string
+    default = "vmep"
+}
+variable "azurerm_virtual_machine_vm_size" {
+    description = "(Required) Specifies the size of the Virtual Machine."
+    type = string
+    default = "Standard_B2s"
+}
+variable "storage_image_reference_publisher" {
+    description = "(Required) Specifies the publisher of the image used to create the virtual machine. Changing this forces a new resource to be created."
+    type = string
+    default = "MicrosoftWindowsServer"
+}
+variable "storage_image_reference_offer" {
+    description = " (Required) Specifies the offer of the image used to create the virtual machine. Changing this forces a new resource to be created."
+    type = string
+    default = "WindowsServer"
+}
+variable "storage_image_reference_sku" {
+    description = "(Required) Specifies the SKU of the image used to create the virtual machine. Changing this forces a new resource to be created."
+    type = string
+    default = "2019-Datacenter"
+}
+variable "storage_image_reference_version" {
+    description = "(Optional) Specifies the version of the image used to create the virtual machine. Changing this forces a new resource to be created."
+    type = string
+    default = "latest"
+}
+variable "storage_os_disk_name" {
+    description = "storage os disk name"
+    type = string
+    default = "server-os"
+}
+variable "storage_os_disk_caching" {
+    description = "(Optional) Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
+    type = string
+    default = "ReadWrite"
+}
+variable "storage_os_disk_create_option" {
+    description = " (Required) Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+    type = string
+    default = "FromImage"
+}
+variable "storage_os_disk_managed_disk_type" {
+    description = "(Optional) Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+    type = string
+    default = "Standard_LRS"
+}
+
+variable "os_profile_computer_name" {
+    description = "(Required) Specifies the name of the Virtual Machine."
+    type = string
+    default = "vmep"
+}
+variable "os_profile_admin_username" {
+    description = "(Required) Specifies the name of the local administrator account."
+    type = string
+    default = "adminUsername"
+}
+variable "os_profile_admin_password" {
+    description = "(Optional) Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+    type = string
+    default = "Passw0rd1234"
+}
+variable "os_profile_windows_config_provision_vm_agent" {
+    description = "(Optional) Should the Azure Virtual Machine Guest Agent be installed on this Virtual Machine? Defaults to false."
+    type = bool
+    default = true
+}
+variable "os_profile_windows_config_protocol" {
+    description = "(Required) Specifies the protocol of listener. Possible values are HTTP or HTTPS."
+    type = string
+    default = "HTTP" 
+}
